68bbbd3af32f37c0c87b86ae486f6356a36581b1
Mike Treit <mtreit@ntdev.microsoft.com>
2022-09-06 14:46:45 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Test
+{
+    interface ISomeInterface
+    {
+        public void DoSomething();
+    }
+
+    public sealed class TestClass : IDisposable
+    {
+        private readonly ChannelFactory<ISomeInterface> channelFactory;
+
+        public void Dispose()
+        {
+            channelFactory.Dispose();
+        }
+    }
+}
